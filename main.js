219bdfa3c453368692997c5c45e22c51cf898c9d
zokaiG <futlouco7@gmail.com>
2020-02-06 11:11:08 -0500

    This is everything learned until lesson 7 on Computed Properties. Right before usage of components

---
+++ main.js
@@ -0,0 +1,50 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        selectedVariant: 0,
+        product: 'Socks',
+        brand: 'Vue Mastery',
+        description: 'A pair of warm, fuzzy socks',
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+          {
+            variantId: 2234,
+            variantColor: "green",
+            variantImage: './images/vmSocks-green-onWhite.jpg',
+            variantQuantity: 10
+          },
+          {
+            variantId: 2235,
+            variantColor: "blue",
+            variantImage: './images/vmSocks-blue-onWhite.jpg',
+            variantQuantity: 0
+          }
+        ],
+        sizes: ['S', 'M', 'L'],
+        cart: 0
+    },
+    methods: {
+        addToCart: function(){
+            this.cart += 1
+        },
+       subToCart: function(){
+            this.cart -= 1
+        },
+        updateProduct(index){
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title: function(){
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        }
+    }
+})
