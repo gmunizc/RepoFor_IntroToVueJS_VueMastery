0e532892bf8e10597db4444fa0c07d1ac0454182
zokaiG <futlouco7@gmail.com>
2020-02-06 22:23:23 -0500

    - Finished lesson 10 on Forms.

--- main.js
+++ main.js
@@ -1,3 +1,79 @@
+Vue.component('product-review',{
+    template: `
+        <form class="review-form" @submit.prevent="onSubmit">
+            <p v-if="errors.length">
+                <b>Please correct the following error(s):</b>
+                <ul>
+                    <li v-for="error in errors">{{error}}</li>
+                </ul>
+            </p>
+            <p>
+                <label for="name">Name:</label>
+                <input id="name" v-model="name">
+            </p>
+            <p>
+                <label for="review">Review:</label>
+                <textarea id="review" v-model="review"></textarea>
+            </p>
+            <p>
+                <label for="rating">Rating:</label>
+                <select id="rating" v-model.number="rating">
+                    <option>5</option>
+                    <option>4</option>
+                    <option>3</option>
+                    <option>2</option>
+                    <option>1</option>
+                </select>
+            </p>
+            <p>Would you recommend this product?</p>
+            <label>
+                Yes
+                <input type="radio" value="Yes" v-model="recommend"/>
+            </label>
+            <label>
+                No
+                <input type="radio" value="No" v-model="recommend"/>
+            </label>
+            <p>
+                <input type="submit" value="Submit">
+            </p>
+        </form>
+    `,
+    data(){
+        return {
+            name:null,
+            review: null,
+            rating: null,
+            recommend: null,
+            errors: []
+        }
+    },
+    methods:{
+        onSubmit(){
+            this.errors = []
+            if(this.name && this.review && this.rating && this.recommend){
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating,
+                    recommend: this.recommend
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null,
+                this.review = null,
+                this.rating = null,
+                this.recommend = null
+            }
+            else{
+                if(!this.name) this.errors.push("Name required.")
+                if(!this.review) this.errors.push("Review required.")
+                if(!this.rating) this.errors.push("Rating required.")
+                if(!this.recommend) this.errors.push("Recommendation required.")
+            }
+        }
+    }
+})
+
 Vue.component('product-details', {
     props: {
         details: {
@@ -55,6 +131,23 @@ Vue.component('product', {
 					<p><a :href="link" target="_blank">More products like this</a></p>
 				</div>
 
+                <div>
+                    <h2>Reviews</h2>
+                    <p v-if="!reviews.length">There are no reviews yet.</p>
+                    <ul>
+                        <li v-for="review in reviews">
+                            <ul>
+                                <li>{{review.name}}</li>
+                                <li>Rating: {{review.rating}}</li>
+                                <li>{{review.review}}</li>
+                                <li>Recommend? {{review.recommend}}</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+
+				<product-review @review-submitted="addReview"></product-review>
+
 			</div>
     `,
     data(){
@@ -80,7 +173,8 @@ Vue.component('product', {
                 variantQuantity: 0
               }
             ],
-            sizes: ['S', 'M', 'L']
+            sizes: ['S', 'M', 'L'],
+            reviews: []
         }
     },
     methods: {
@@ -95,6 +189,9 @@ Vue.component('product', {
 //        },
         updateProduct(index){
             this.selectedVariant = index
+        },
+        addReview(productReview){
+            this.reviews.push(productReview)
         }
     },
     computed: {
